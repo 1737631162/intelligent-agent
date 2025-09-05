73c63f791031496d07190ea86ca5cebd12386b75
SterlingLeo <49341242+1737631162@users.noreply.github.com>
2025-09-05 21:14:53 +0800

    Create AgentIntentionRecognitionQuestionResultDTO.java

---
+++ vbot-intelligent-agent-server/src/main/java/com/changan/vbot/service/dto/AgentIntentionRecognitionQuestionResultDTO.java
@@ -0,0 +1,8 @@
+package com.changan.vbot.service.dto;
+
+import lombok.Data;
+
+@Data
+public class AgentIntentionRecognitionQuestionResultDTO extends AgentIntentionRecognitionResultDTO{
+    private Integer label;
+}
