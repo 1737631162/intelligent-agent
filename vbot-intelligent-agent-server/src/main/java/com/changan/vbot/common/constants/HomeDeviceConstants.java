6b3469acf96a6d1c9d5e378e1843a4c518aca42a
SterlingLeo <49341242+1737631162@users.noreply.github.com>
2025-09-05 21:28:13 +0800

    Create HomeDeviceConstants.java

---
+++ vbot-intelligent-agent-server/src/main/java/com/changan/vbot/common/constants/HomeDeviceConstants.java
@@ -0,0 +1,11 @@
+package com.changan.vbot.common.constants;
+
+public interface HomeDeviceConstants {
+    String ACTION = "action";
+    String BRAND = "brand";
+    String LOC = "loc";
+    String INDEX = "index";
+    String TARGET_NAME = "targetName";
+    String LIVING = "living";
+    String IS_ALL = "isAll";
+}
